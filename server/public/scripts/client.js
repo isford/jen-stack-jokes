e90f6cca3a23ae7c12e92e167d1d0e02d87fefe0
Ian <isford7@gmail.com>
2021-05-24 13:49:10 -0500

    Added notes to basecode

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -7,7 +7,7 @@ function onReady() {
 
     //click event to add jokes to POST to server
     $('#addJokeButton').on('click', addJokes);
-
+    //updates dom to display newest jokes
     getJokes();
 }
 
@@ -40,9 +40,9 @@ function getJokes(){
         url: '/jokes'
     }).then(response => {
         console.log('The jokes array is', response);
-
+        //clears the div so you don't have duplicates
         $('#outputDiv').empty();
-
+        //loops through array and appends all jokes
         for (let joke of response){
             $('#outputDiv').append(`
             <h3> ${joke.whoseJoke} ${joke.jokeQuestion} ${joke.punchLine} </h3>
--- server/server.js
+++ server/server.js
@@ -6,6 +6,8 @@ const PORT = 5000;
 // use bodyParser.urlencoded throughout the app with this:
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+//Jokes array created for displaying on DOM after page refresh
 let jokes = [
   {
     whoseJoke: "Danny",
@@ -33,7 +35,7 @@ let jokes = [
     punchLine: "It was a shih tzu."
   }
 ];
-
+//POST input from client to server
 app.post('/jokes', (req, res) => {
     console.log('Received from user input', req.body);
 
@@ -41,7 +43,7 @@ app.post('/jokes', (req, res) => {
     let question = req.body.question;
     let punchLine = req.body.punchline;
 
-
+  //create a new object to add to array
     let newJoke = {
       whoseJoke: whoseJoke,
       jokeQuestion: question,
@@ -51,10 +53,10 @@ app.post('/jokes', (req, res) => {
     console.log('The users joke to POST', newJoke)
 
     jokes.push(newJoke);
-
+    //send status code to let client know data was received
     res.sendStatus(200);
   })
-
+//GET to send jokes array to client for display on DOM
 app.get('/jokes', (req, res) => {
     console.log('Got to /jokes');
 
